f3ac8fa65778f0e755f0bf23a89286a5258f57a0
AissaKifouche <a_kifouche@estin.dz>
2026-08-10 16:11:15 +0100

    add a function to return area infos based on position (lat and lon)

--- lib/src/location_service.dart
+++ lib/src/location_service.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart' ;
 
 class LocationService {
 
@@ -20,6 +22,30 @@ class LocationService {
     }catch (e) {
       return null;
     }
+  }
+
+  static Future<String> reverseGeocode(double lat, double lon) async {
+    try {
+      final placemarks = await Geocoding().placemarkFromCoordinates(lat, lon);
+      if (placemarks.isNotEmpty) {
+        final place = placemarks.first;
+
+        // Check if values are non-null and non-empty
+        if (place.locality != null && place.locality!.trim().isNotEmpty) {
+          return place.locality!;
+        }
+        if (place.subAdministrativeArea != null && place.subAdministrativeArea!.trim().isNotEmpty) {
+          return place.subAdministrativeArea!;
+        }
+        if (place.administrativeArea != null && place.administrativeArea!.trim().isNotEmpty) {
+          return place.administrativeArea!;
+        }
+      }
+    } catch (e) {
+      debugPrint("Error in reverse geocoding: $e");
+    }
+
+    return 'Unknown Location';
   }
 
 }
